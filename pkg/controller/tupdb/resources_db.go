16f932658db07b25917ad1f4de0ddc3638bebff5
JINwonLEE <dlwlsdnjsehs1993@gmail.com>
2020-09-15 16:57:12 +0900

    [bugfix] 무한 requeue 되는 버그 수정
    - Ingress가 정상적으로 동작되었을 때 무한 requeue되는 버그 수정

--- pkg/controller/tupdb/resources_db.go
+++ pkg/controller/tupdb/resources_db.go
@@ -170,6 +170,9 @@ func dbIngress(dbInstance *tmaxv1.TupDB) (*networkingv1beta1.Ingress, error) {
 }
 
 func checkIngressAndUpdate(ingInstance *networkingv1beta1.Ingress) bool {
+	if (ingInstance.Spec.Rules[0].Host != IngressDefault) {
+		return true
+	}
 	if len(ingInstance.Status.LoadBalancer.Ingress) != 0 && len(ingInstance.Spec.Rules) == 1 && ingInstance.Spec.Rules[0].Host == IngressDefault {
 		ingInstance.Spec.Rules[0].Host = fmt.Sprintf("%s.%s.%s.nip.io", ingInstance.Name, ingInstance.Namespace, ingInstance.Status.LoadBalancer.Ingress[0].IP)
 		return true
