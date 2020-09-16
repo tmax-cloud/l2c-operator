a2169d1d75e60b86b3f71452905d824f547900dc
Sunghyun Kim <cqbqdd11519@gmail.com>
2020-09-16 17:24:33 +0900

    [refactor] Move blocks of codes into proper location

--- pkg/controller/tupwas/tupwas_controller.go
+++ pkg/controller/tupwas/tupwas_controller.go
@@ -187,13 +187,22 @@ func (r *ReconcileTupWAS) Reconcile(request reconcile.Request) (reconcile.Result
 		return reconcile.Result{}, err
 	}
 
-	// If it is ready (only once when analyze is not executed at all) and not analyzing, launch analyze once
-	readyCond, readyCondFound := instance.Status.GetCondition(tmaxv1.WasConditionKeyProjectReady)
-	analyzeCond, analyzeCondFound := instance.Status.GetCondition(tmaxv1.WasConditionKeyProjectAnalyzing)
-	if readyCondFound && analyzeCondFound && instance.Status.LastAnalyzeStartTime == nil && readyCond.Status == corev1.ConditionTrue && analyzeCond.Status == corev1.ConditionFalse {
-		pr := AnalyzePipelineRun(instance)
-		if err := r.createAndUpdateStatus(pr, instance, "cannot create pipelineRun"); err != nil {
-			return reconcile.Result{}, err
+	// Update ingress - apply host
+	wasIngress := &networkingv1beta1.Ingress{}
+	err := r.client.Get(context.TODO(), types.NamespacedName{Name: instance.GenWasResourceName(), Namespace: instance.Namespace}, wasIngress)
+	if err != nil && !errors.IsNotFound(err) {
+		instance.Status.WasUrl = ""
+		return reconcile.Result{}, err
+	} else if err == nil {
+		if len(wasIngress.Status.LoadBalancer.Ingress) != 0 && len(wasIngress.Spec.Rules) == 1 && wasIngress.Spec.Rules[0].Host == IngressDefaultHost {
+			// If Loadbalancer is given to the ingress, but host is not set, set host!
+			wasIngress.Spec.Rules[0].Host = fmt.Sprintf("%s.%s.%s.nip.io", instance.Name, instance.Namespace, wasIngress.Status.LoadBalancer.Ingress[0].IP)
+			if err := r.client.Update(context.TODO(), wasIngress); err != nil {
+				return reconcile.Result{}, err
+			}
+		} else if len(wasIngress.Spec.Rules) == 1 && wasIngress.Spec.Rules[0].Host != IngressDefaultHost {
+			// Update ingress url to a status field
+			instance.Status.WasUrl = fmt.Sprintf("http://%s", wasIngress.Spec.Rules[0].Host)
 		}
 	}
 
--- pkg/controller/tupwas/tupwas_controller_resource.go
+++ pkg/controller/tupwas/tupwas_controller_resource.go
@@ -1,15 +1,11 @@
 package tupwas
 
 import (
-	"context"
 	"fmt"
 	tektonv1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
 	"github.com/tmax-cloud/l2c-operator/internal/utils"
 	tmaxv1 "github.com/tmax-cloud/l2c-operator/pkg/apis/tmax/v1"
 	corev1 "k8s.io/api/core/v1"
-	networkingv1beta1 "k8s.io/api/networking/v1beta1"
-	"k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/types"
 )
 
 func (r *ReconcileTupWAS) deployResources(instance *tmaxv1.TupWAS) error {
@@ -116,22 +112,13 @@ func (r *ReconcileTupWAS) deployResources(instance *tmaxv1.TupWAS) error {
 		}
 	}
 
-	// Update ingress - apply host
-	wasIngress := &networkingv1beta1.Ingress{}
-	err = r.client.Get(context.TODO(), types.NamespacedName{Name: instance.GenWasResourceName(), Namespace: instance.Namespace}, wasIngress)
-	if err != nil && !errors.IsNotFound(err) {
-		instance.Status.WasUrl = ""
-		return err
-	} else if err == nil {
-		if len(wasIngress.Status.LoadBalancer.Ingress) != 0 && len(wasIngress.Spec.Rules) == 1 && wasIngress.Spec.Rules[0].Host == IngressDefaultHost {
-			// If Loadbalancer is given to the ingress, but host is not set, set host!
-			wasIngress.Spec.Rules[0].Host = fmt.Sprintf("%s.%s.%s.nip.io", instance.Name, instance.Namespace, wasIngress.Status.LoadBalancer.Ingress[0].IP)
-			if err := r.client.Update(context.TODO(), wasIngress); err != nil {
-				return err
-			}
-		} else if len(wasIngress.Spec.Rules) == 1 && wasIngress.Spec.Rules[0].Host != IngressDefaultHost {
-			// Update ingress url to a status field
-			instance.Status.WasUrl = fmt.Sprintf("http://%s", wasIngress.Spec.Rules[0].Host)
+	// If it is ready (only once when analyze is not executed at all) and not analyzing, launch analyze once
+	readyCond, readyCondFound := instance.Status.GetCondition(tmaxv1.WasConditionKeyProjectReady)
+	analyzeCond, analyzeCondFound := instance.Status.GetCondition(tmaxv1.WasConditionKeyProjectAnalyzing)
+	if readyCondFound && analyzeCondFound && instance.Status.LastAnalyzeStartTime == nil && readyCond.Status == corev1.ConditionTrue && analyzeCond.Status == corev1.ConditionFalse {
+		pr := AnalyzePipelineRun(instance)
+		if err := r.createAndUpdateStatus(pr, instance, "cannot create pipelineRun"); err != nil {
+			return err
 		}
 	}
 
